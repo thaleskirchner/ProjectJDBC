010b9ddb373bb1eb65d5c717b1bd8d426bd7de64
Thales Kirchner Ozéias <thales.ozeias@gmail.com>
2026-04-13 20:39:27 -0300

    Creation of Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department dp = new Department(1, "Books");
 		System.out.println(dp);
+		
+		Seller seller = new Seller(1,"Alex", "alex@gmail.com", new Date(), 2000.00, dp);
+		System.out.println(seller);
 	}
 
 }
