c169b0bb5e82757e054169b15994a73c16a59db4
Thales Kirchner Ozéias <thales.ozeias@gmail.com>
2026-04-13 20:59:56 -0300

    Creation of SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,9 @@ public class Program {
 		System.out.println(dp);
 		
 		Seller seller = new Seller(1,"Alex", "alex@gmail.com", new Date(), 2000.00, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 
